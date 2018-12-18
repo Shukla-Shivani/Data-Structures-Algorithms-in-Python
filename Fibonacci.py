89d763b5cb292270b6e241ab914f97a4d10c665e
Shivani Shukla <shukla.shivani2009@gmail.com>
2018-12-18 16:34:57 +0530

    recursive fibonacci implementation

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+"""Implement a function recursively to get the desired
+Fibonacci sequence value."""
+
+def get_fib(position):
+    if position == 0 or position == 1:
+        return position
+    else:
+        output = get_fib(position - 1) + get_fib(position - 2)
+        return output
+    return -1
+
+# Test cases
+print get_fib(9)
+print get_fib(11)
+print get_fib(0)
