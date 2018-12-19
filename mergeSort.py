c1a9474e95f800bde99a328b5c9c21bc55813251
Shivani Shukla <shukla.shivani2009@gmail.com>
2018-12-19 09:54:29 +0530

    updating merge sort

--- mergeSort.py
+++ mergeSort.py
@@ -2,54 +2,41 @@
 Input a list.
 Output a sorted list."""
 
-"function to merge two sub arrays"
-def merge(left, right, array):
-    i = 0
-    j = 0
-    k = 0
-    mergeSort(left)
-    mergeSort(right)
-
-    while (i < len(left) and j < len(right)):
-        if (left[i] < right[j]):
-            array[k] = left[i]
-            i = i + 1
-        else:
-            array[k] = right[j]
-            j = j + 1
-        k = k + 1
-
-    while (i < len(left)):
-        array[k] = left[i]
-        i = i + 1
-        k = k + 1
-
-    while (j < len(right)):
-        array[k] = right[j]
-        j = j + 1
-        k = k + 1
-
-"function to divide and merge input array"
 def mergeSort(array):
+    if len(array)>1:
+        mid = len(array) // 2
+        L = array[:mid]
+        R = array[mid:]
 
-    n = len(array)
-    mid = n//2
-    left = []
-    right = []
+        mergeSort(L)
+        mergeSort(R)
 
-    for i in range(mid):
-        number = array[i]
-        left.append(number)
+        i = 0
+        j = 0
+        k = 0
 
-    for i in range(mid, n):
-        number = array[i]
-        right.append(number)
+        while i<len(L) and j<len(R):
+            if L[i]<R[j]:
+                array[k] = L[i]
+                i = i+1
+            else:
+                array[k] = R[j]
+                j = j+1
+            k = k+1
 
-#mergeSort(left)
-#mergeSort(right)
-    merge(left, right, array)
+        while i<len(L):
+            array[k] = L[i]
+            i = i+1
+            k = k+1
+
+        while j<len(R):
+            array[k] = R[j]
+            j = j+1
+            k = k+1
+
+    return array
+
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
-mergeSort(test)
-for i in test:
-    print(i)
+print(mergeSort(test))
+
